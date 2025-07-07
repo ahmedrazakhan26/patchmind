c6138b1cab4c17b6b3599867a3565869dc0c80ad
4EverHonk <81518236+Darkstar420@users.noreply.github.com>
2025-07-07 16:22:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='patchmind',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(exclude=('tests',)),
     install_requires=[
         'GitPython',
